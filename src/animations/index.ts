3c76cb35dac3f36fb7956661585b9e25d2e123f4
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-18 15:46:29 +0530

    make fade container animation

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -7,3 +7,14 @@ export const Fade: Variants = {
     opacity: 1
   }
 };
+
+export const FadeContainer: Variants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.2
+    }
+  }
+};
