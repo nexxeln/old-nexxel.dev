1f1e038956e145b5482a5ae47449f4a3fb63651f
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-18 20:12:24 +0530

    give some padding

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -5,8 +5,10 @@ const Wrapper: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className="flex flex-col items-center">
       <div className="mt-10 max-w-95 lg:max-w-60 2xl:max-w-40 xl:max-w-30">
-        <Navbar />
-        <div className="pt-16">
+        <div className="px-5">
+          <Navbar />
+        </div>
+        <div className="px-5 pt-16">
           <main id="main">{children}</main>
         </div>
       </div>
