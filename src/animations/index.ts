d0eec9d76ebf76f50a51f7a999e33ed73c2972ba
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-18 17:21:53 +0530

    make pfp animation

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -18,3 +18,16 @@ export const FadeContainer: Variants = {
     }
   }
 };
+
+export const Pfp: Variants = {
+  hidden: { scale: 0, opacity: 0 },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      stiffness: 260,
+      damping: 15
+    }
+  }
+};
