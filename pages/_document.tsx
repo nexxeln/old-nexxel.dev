bf002005c330583e2a0cfff41f4d2087b9ff4a6d
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-18 15:16:08 +0530

    import barlow font from google fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class PortfolioDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600;700&display=swap"
+            rel="stylesheet"
+            as="font"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
