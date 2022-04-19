00711a780a88acab98994887cbc5456491640e42
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-19 21:04:48 +0530

    maybe now

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,8 @@ module.exports = {
   content: ["./src/pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Barlow", "regular"]
-      }
+    fontFamily: {
+      sans: ["Barlow", "regular"]
     }
   },
   plugins: []
