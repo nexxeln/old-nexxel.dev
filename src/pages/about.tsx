7af0a5ef7be5971c43199aac7391826d90922a68
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-20 20:21:09 +0530

    foundations of about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Wrapper from "../components/Wrapper";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <Wrapper>
+        <div className="flex items-center justify-start">
+          <Header head="About" />
+        </div>
+      </Wrapper>
+    </>
+  );
 };
 
 export default about;
