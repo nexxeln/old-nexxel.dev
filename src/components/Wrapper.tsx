5a6577da375bd78b90aaea92abd6b50a732865f4
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-20 19:45:54 +0530

    make wrapper reusable

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -5,8 +5,8 @@ import Navbar from "./Navbar";
 
 const Wrapper: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="mt-10 max-w-95 lg:max-w-60 2xl:max-w-40 xl:max-w-30">
+    <div className="flex flex-col md:mx-20 lg:mx-72">
+      <div className="mt-10">
         <div className="px-5">
           <Navbar />
         </div>
