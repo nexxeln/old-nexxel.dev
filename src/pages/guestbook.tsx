05aa05bac40f66537215671cda59f23d1736f2a0
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-21 14:50:18 +0530

    make basic guestbook page

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Text from "../components/Text";
+import Wrapper from "../components/Wrapper";
 
 const guestbook = () => {
-  return <div>guestbook</div>;
+  return (
+    <>
+      <Wrapper>
+        <div className="pb-3">
+          <Header head="Guestbook" />
+        </div>
+        <Text>
+          <div className="text-lg">
+            Leave a comment below to sign my Guestbook. It could be anything.
+            Surprise me!
+          </div>
+        </Text>
+      </Wrapper>
+    </>
+  );
 };
 
 export default guestbook;
