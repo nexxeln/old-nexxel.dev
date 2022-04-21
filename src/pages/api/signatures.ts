42b617bec1fcb53ab543ef6c103c2e5829159ad9
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-21 17:19:47 +0530

    create api route for signatures

---
+++ src/pages/api/signatures.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    return await createSignature(req, res);
+  } else {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+}
+
+async function createSignature(req: NextApiRequest, res: NextApiResponse) {
+  const { body } = req;
+
+  try {
+    const newEntry = await prisma.signatures.create({
+      data: {
+        name: body.name,
+        message: body.message
+      }
+    });
+    return res.status(200).json(newEntry);
+  } catch (error) {
+    console.log("Request error", error);
+    return res.status(500).json({ error: "Error creating signature" });
+  }
+}
