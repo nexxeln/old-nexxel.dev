af6d92fcbc6e44f1bcfca40a4fa666de577ce52f
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-21 15:37:09 +0530

    make header take a size prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,13 @@ import { FC } from "react";
 import { motion } from "framer-motion";
 import { FadeContainer, Fade } from "../animations";
 
-const Header: FC<{ head: string }> = ({ head }) => {
+const Header: FC<{ head: string; size?: number }> = ({ head, size }) => {
   return (
     <motion.div variants={FadeContainer} initial="hidden" animate="visible">
-      <motion.h1 className="text-4xl font-bold" variants={Fade}>
+      <motion.h1
+        className={`${size === 5 ? "text-5xl" : "text-4xl"} font-bold`}
+        variants={Fade}
+      >
         {head}
       </motion.h1>
     </motion.div>
