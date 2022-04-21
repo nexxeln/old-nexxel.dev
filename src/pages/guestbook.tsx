c621ee0756cb6cdd097f3407b98a800d08660a6f
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-21 16:15:34 +0530

    add input field

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
+import Input from "../components/Input";
 import Text from "../components/Text";
 import Wrapper from "../components/Wrapper";
 
@@ -8,14 +9,17 @@ const guestbook = () => {
     <>
       <Wrapper>
         <div className="pb-3">
-          <Header head="Guestbook" />
+          <Header head="Guestbook" size={5} />
         </div>
         <Text>
           <div className="text-lg">
-            Leave a comment below to sign my Guestbook. It could be anything.
-            Surprise me!
+            Leave a comment below to sign my Guestbook. It could literally be
+            anything - a joke, a quote or even a cool fact. Surprise me!
           </div>
         </Text>
+        <div className="mt-6">
+          <Input />
+        </div>
       </Wrapper>
     </>
   );
