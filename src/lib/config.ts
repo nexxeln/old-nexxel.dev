395f1011f10d451fe2128a964739a2f49ccba95b
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-21 22:57:44 +0530

    work?

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://nexxel-dev.vercel.app";
+export const server = dev ? "http://localhost:3000" : "";
