a8b115ab631de3e1806240af9b3e772bf4e226ff
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-22 09:22:09 +0530

    improve light mode

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -27,11 +27,11 @@ const Input = () => {
   return (
     <>
       <form onSubmit={(e) => handleSubmit(e)} className="w-full">
-        <div className="flex items-center w-full text-sm text-black border-2 border-gray-300 rounded-md md:text-lg dark:text-white dark:border-gray-400">
+        <div className="flex items-center w-full text-sm text-black border-2 border-black rounded-md md:text-lg dark:text-white dark:border-gray-400">
           <input
             name="name"
             type="text"
-            className="w-[30%] px-4 py-2 border-r-2 border-gray-300 rounded-l-md focus:outline-none dark:bg-zinc-900 dark:border-gray-400 placeholder:text-gray-400 dark:placeholder:text-gray-300"
+            className="w-[30%] px-4 py-2 border-r-2 border-black rounded-l-md focus:outline-none dark:bg-zinc-900 dark:border-gray-400 placeholder:text-gray-400 dark:placeholder:text-gray-300"
             placeholder="Your name"
             value={form.name}
             onChange={handleChange}
@@ -39,7 +39,7 @@ const Input = () => {
           <input
             name="message"
             type="text"
-            className="w-[70%] px-4 py-2 rounded-r-md focus:outline-non placeholder:text-gray-400 dark:placeholder:text-gray-300 dark:bg-zinc-900"
+            className="w-[70%] px-4 py-2 rounded-r-md focus:outline-none placeholder:text-gray-400 dark:placeholder:text-gray-300 dark:bg-zinc-900"
             placeholder="Your message..."
             value={form.message}
             onChange={handleChange}
@@ -48,7 +48,7 @@ const Input = () => {
         <div className="mt-3" />
         <button
           type="submit"
-          className="px-4 py-2 text-teal-500 transition-all duration-300 border-2 border-teal-600 rounded-md hover:scale-[1.04]"
+          className="px-4 py-2 text-teal-600 transition-all duration-300 border-2 border-teal-600 rounded-md hover:scale-[1.04]"
         >
           Sign
         </button>
