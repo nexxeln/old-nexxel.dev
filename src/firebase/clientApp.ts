2814784d5ed485984947b4f427a730be71f21ede
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-23 10:58:33 +0530

    export auth

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -16,4 +16,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 
-export { app, firestore };
+export { app, auth, firestore };
