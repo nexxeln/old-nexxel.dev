f5e46c10bf5c7aea83d29f0c2c9ff8b987402825
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-02 11:51:59 +0530

    fix margins for mobile

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -67,7 +67,7 @@ const Intro: FC = () => {
         <Wavy
           text="Highschooler and Developer"
           heading="p"
-          className="mt-3 mb-10 text-xl text-gray-900 dark:text-white"
+          className="mt-11 md:mt-3 mb-10 text-xl text-gray-900 dark:text-white"
         />
 
         <motion.p
