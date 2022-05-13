9eb9946f786b5d95a3dfccabc0fba2a0a6e2a91c
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-14 01:14:29 +0530

    fix padding

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -62,12 +62,10 @@ const Input = ({ user }: InputProps) => {
             onChange={handleChange}
           />
         </div>
-        <p className="flex justify-end items-end pr-2 text-sm">
+        <p className="flex justify-end items-end pr-2 pt-1 text-sm">
           {message.length}/100
         </p>
-        <p className="pt-2 text-sm text-gray-500 dark:text-gray-400">
-          - {name}
-        </p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">- {name}</p>
         <div className="flex" />
         <div className="pb-2" />
         <Button
